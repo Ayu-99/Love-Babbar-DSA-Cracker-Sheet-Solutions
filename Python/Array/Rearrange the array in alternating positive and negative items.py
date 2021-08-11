95bd4c1efd131e81e440aca964425e20d40e198d
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2021-08-11 16:36:06 +0530

    Update Rearrange the array in alternating positive and negative items.py

--- Python/Array/Rearrange the array in alternating positive and negative items.py
+++ Python/Array/Rearrange the array in alternating positive and negative items.py
@@ -1,17 +1,8 @@
-# Python3 program to rearrange array
-# in alternating positive & negative
-# items with O(1) extra space
 
-# Function to rearrange positive and
-# negative integers in alternate fashion.
-# The below solution does not maintain
-# original order of elements
 def rearrange(arr, n):
 	i = 0
 	j = n - 1
 
-	# shift all negative values
-	# to the end
 	while (i < j):
 	
 		while (i <= n - 1 and arr[i] > 0):
@@ -24,34 +15,23 @@ def rearrange(arr, n):
 			arr[i] = arr[j]
 			arr[j] = temp
 			
-	# i has index of leftmost
-	# negative element
 	if (i == 0 or i == n):
 		return 0
 
-	# start with first positive element
-	# at index 0
-
-	# Rearrange array in alternating
-	# positive & negative items
 	k = 0
 	while (k < n and i < n):
 		
-		# swap next positive element at even
-		# position from next negative element.
 		temp = arr[k]
 		arr[k] = arr[i]
 		arr[i] = temp
 		i = i + 1
 		k = k + 2
 
-# Utility function to print an array
 def printArray(arr, n):
 	for i in range(n):
 		print(arr[i], end = " ")
 	print("\n")
 
-# Driver code
 arr = [2, 3]
 
 n = len(arr)
@@ -63,6 +43,3 @@ rearrange(arr, n)
 
 print( "Rearranged array is")
 printArray(arr, n)
-
-# This code is contributed
-# Princi Singh
