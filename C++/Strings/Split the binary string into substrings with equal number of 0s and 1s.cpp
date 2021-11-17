6e5aef467a157ecaaea8d291a348736a7c55136d
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2021-11-17 20:08:14 +0530

    Create Split the binary string into substrings with equal number of 0s and 1s.cpp

---
+++ C++/Strings/Split the binary string into substrings with equal number of 0s and 1s.cpp
@@ -0,0 +1,41 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int maxSubStr(string str, int n)
+{
+
+	int count0 = 0, count1 = 0;
+
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		if (str[i] == '0') {
+			couqnt0++;
+		}
+		else {
+			count1++;
+		}
+		if (count0 == count1) {
+			cnt++;
+            count0 = 0;
+            count1 = 0;
+		}
+	}
+
+	if (cnt == 0) {
+		return -1;
+	}
+
+	return cnt;
+}
+
+int main()
+{
+	string str = "0100110101";
+	int n = str.length();
+
+	cout << maxSubStr(str, n);
+
+	return 0;
+}
